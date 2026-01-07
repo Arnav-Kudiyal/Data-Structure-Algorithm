9bc9e72748cebebbca35c076e196373a614062d1
Arnav Kudiyal <reacharnavkudiyal@gmail.com>
2026-01-07 16:38:41 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,62 @@
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int marks[5];
+    int size = sizeof(marks) / sizeof(int);
+
+    for(int i = 0; i<=size; i++)
+    {
+        cin >> marks[i];
+    }
+
+
+    for(int i=0; i<size; i++)
+    {
+        cout << marks[i] << endl;
+    }
+    return 0;
+}
+
+*/
+
+/*
+#include<iostream>
+using namespace std;
+
+void changeArray(int arr[], int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        cout << "Array in function" << arr[i] << endl;
+    }
+    for(int i=0; i<size; i++)
+    {
+        arr[i] = arr[i] * 2;
+        cout << arr[i] << endl;
+
+    }
+}
+
+int main()
+{
+    int numbers[5] = {1,2,3,4,5};
+    int size = 5;
+
+    changeArray(numbers, size);
+
+    return 0;
+}
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
